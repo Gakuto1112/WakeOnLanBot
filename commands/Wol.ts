40fbb086dcc5232bd06584cef775074ce75bf04c
Gakuto1112 <gakuto131112@gmail.com>
2022-08-04 19:05:57 +0900

    PCが既に起動している場合はwolを発信しないように変更

--- commands/Wol.ts
+++ commands/Wol.ts
@@ -1,19 +1,22 @@
 import {Command} from "./Command";
-import {colors,settings} from "../WakeOnLanBot";
+import {colors, settings, isPCAwake} from "../WakeOnLanBot";
 import * as child_process from "child_process";
 import {BaseCommandInteraction} from "discord.js";
 
 export class WolCommand extends Command {
 	public async run(interaction: BaseCommandInteraction): Promise<void> {
-		console.info("マジックパケットを送信します。");
-		await interaction.reply(":loudspeaker: マジックパケットを送信します");
-		child_process.exec("sudo etherwake " + settings.targetMacAddress, async (error: child_process.ExecException | null, stdout: string, stderr: string) => {
-			if(error) {
-				console.group(colors.red + "マジックパケットの送信に失敗しました。" + colors.reset);
-				console.error(stderr);
-				console.groupEnd();
-				await interaction.followUp(":x: マジックパケットの送信に失敗しました\n" + stderr);
-			}
-		});
+		if(isPCAwake) await interaction.reply(":sunny: コンピューターは既に起動しています。")
+		else {
+			console.info("マジックパケットを送信します。");
+			await interaction.reply(":loudspeaker: マジックパケットを送信します");
+			child_process.exec("sudo etherwake " + settings.targetMacAddress, async (error: child_process.ExecException | null, stdout: string, stderr: string) => {
+				if(error) {
+					console.group(colors.red + "マジックパケットの送信に失敗しました。" + colors.reset);
+					console.error(stderr);
+					console.groupEnd();
+					await interaction.followUp(":x: マジックパケットの送信に失敗しました\n" + stderr);
+				}
+			});
+		}
 	}
 }
